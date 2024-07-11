77428f1c1b1ceb6a631ee3d2b71067a882d303e9
DIma <barth0l0mew@mail.ru>
2024-07-11 21:32:31 +0300

    Springboot 2

--- helloWorld/src/main/java/org/itstep/app1/Application.java
+++ helloWorld/src/main/java/org/itstep/app1/Application.java
@@ -2,14 +2,31 @@ package org.itstep.app1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-//1 вариант
+//1 вариант есть @Controller и @RestController
 @RestController
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
+    }
+    @RequestMapping ("/")
+private String helloWorld(){
+        return "<h1>hello springboot</h1>";
+    }
+    // в mapping не должно быть одинаковых путей
+    @RequestMapping ("/{name}")
+    private String helloName(@PathVariable("name") String name){
+        return String.format("<h1>hello springboot %s </h1>",name);
+    }
+    //http://localhost:8080/query?name=ddd
+    @RequestMapping ("/query")
+    private String helloName2(@RequestParam("name") String name){
+        return String.format("<h1>hello springboot %s </h1>",name);
     }
     /*
     //2 вариант
---
+++ helloWorld/src/main/java/org/itstep/app2/PersonController.java
@@ -0,0 +1,78 @@
+package org.itstep.app2;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+// добавление в начале пути
+@RequestMapping("/persons")
+
+public class PersonController {
+    private List<Person> persons = new ArrayList<>();
+    {
+        persons.add(new Person(1L, "ivan", "ivanov"));
+        persons.add(new Person(2L, "ivan2", "ivanov2"));
+        persons.add(new Person(3L, "ivan3", "ivanov3"));
+
+    }
+    //Только get запросы
+    @GetMapping
+    //или @GetMapping({"","/"})
+    public List<Person> getAllPerson (){
+        return persons;
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> getAllPerson (@PathVariable("id") Long id){
+        Person person = persons.stream()
+                .filter(p->p.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+        if (person == null) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(person, HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<Person> createPerson (@RequestBody Person person){
+       persons.add(person);
+        return new ResponseEntity<>(person, HttpStatus.CREATED);
+    }
+}
+
+/*
+  fetch('
+http://localhost:8080/users
+', {
+    method: 'GET'
+  })
+  .then(response=>response.json());
+
+fetch('http://localhost:8080/persons', {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({"id":"5L","name": "Jane", "surname":"janesurname"})
+          });
+
+  fetch('
+http://localhost:8080/persons/1
+', {
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({"name":"noname"})
+  });
+
+  fetch('
+http://localhost:8080/persons/1
+', {
+    method: 'DELETE'
+  });
+ */
