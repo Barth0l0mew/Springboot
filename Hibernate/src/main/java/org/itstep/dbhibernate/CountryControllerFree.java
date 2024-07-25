bb7dbbcac6765c6ec8889c3b9ae3667e21436029
DIma <barth0l0mew@mail.ru>
2024-07-25 21:27:26 +0300

    h2 + hibernate

--- Hibernate/src/main/java/org/itstep/dbhibernate/CountryControllerFree.java
+++ Hibernate/src/main/java/org/itstep/dbhibernate/CountryControllerFree.java
@@ -1,42 +1,52 @@
 package org.itstep.dbhibernate;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
-@RestController
-@RequestMapping("/api")
-public class CountryController {
+@Controller
+
+public class CountryControllerFree {
     @Autowired
     public CountryService countryService;
     @GetMapping("/countries/all")
-    public List<Country> getCountries(){
+    public ModelAndView getCountries(){
         var countries = countryService.findAll();
-        return countries;
+        var params = new HashMap<String, Object>();
+        params.put("countries",countries);
+        return new ModelAndView("showCountries",params);
     }
     @GetMapping ("/countries/a")
-    public List<Country> getCountriesA(){
+    public ModelAndView getCountriesA(){
         var countries = countryService
                 .findAll()
                 .stream()
                 .filter(country -> country.getName().charAt(0)=='A')
                 .collect(Collectors.toList());
-        return countries;
+        var params = new HashMap<String, Object>();
+        params.put("countries",countries);
+        return new ModelAndView("showCountriesA",params);
     }
     @GetMapping("/countries/page")
-    public List<Country> getCountriesPage(@RequestParam(defaultValue = "0" ,name="page") int page,
+    public ModelAndView getCountriesPage(@RequestParam(defaultValue = "0" ,name="page") int page,
                                           @RequestParam(defaultValue = "10",name = "size") int size){
         //http://localhost:8080/countries/page
         //http://localhost:8080/countries/page?page=1&size=20
         var countries = countryService
                 .findAll(page, size);
 
-        return countries;
+        var params = new HashMap<String, Object>();
+        params.put("countries",countries);
+        return new ModelAndView("showCountriesPage",params);
     }
 
 }
